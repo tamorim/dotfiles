d6671d14ab0ec27d3587fd96d9ed5bc0642a5d63
Thor Amorim <thoramorim@gmail.com>
2015-08-03 02:13:12 -0300

    Remove .emacs.d folder

--- .emacs.d/elpa/flx-20140921.739/flx-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; flx-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("flx-pkg.el" "flx.el") (21937 47678 381829
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flx-autoloads.el ends here
