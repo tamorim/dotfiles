8fd11077ec7d99946aca27105335019e975877d5
Thor Amorim <thoramorim@gmail.com>
2016-11-05 18:57:42 -0200

    Improve network script to display eth and wifi

--- .scripts/network.sh
+++ .scripts/network.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+eth_icon=""
+wifi_icon=""
+
 read lo int1 int2 <<< `ip link | sed -n 's/^[0-9]: \(.*\):.*$/\1/p'`
 
-if iwconfig $int1 >/dev/null 2>&1; then
+if iwconfig $int1 > /dev/null 2>&1; then
   wifi=$int1
   eth0=$int2
 else
@@ -9,13 +12,16 @@ else
   eth0=$int1
 fi
 
-ip link show $eth0 | grep 'state UP' >/dev/null && int=$eth0 ||int=$wifi
-
-if [[ $int =~ ^w ]]; then
-  int=""
+if ping -I $eth0 -c 1 8.8.8.8 > /dev/null 2>&1; then
+  message="${eth_icon}   connected"
 else
-  int=""
+  message="${eth_icon}   disconnected"
 fi
 
-ping -c 1 8.8.8.8 >/dev/null 2>&1 &&
-  echo "$int   connected" || echo "$int    disconnected"
+if ping -I $wifi -c 1 8.8.8.8 > /dev/null 2>&1; then
+  message="${message}\n\n${wifi_icon}   connected"
+else
+  message="${message}\n\n${wifi_icon}   disconnected"
+fi
+
+echo "$message"
