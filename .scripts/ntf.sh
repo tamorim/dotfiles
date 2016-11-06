5812c655f83afd938db94f465a5b5333befe1379
Thor Amorim <thoramorim@gmail.com>
2016-11-06 06:52:48 -0200

    Fix ntf cat for id file

--- .scripts/ntf.sh
+++ .scripts/ntf.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 cmd=$(sed -n 's/^\(.*\)\s\?.*$/\1/p' <<< "$1")
 id_file="~/.dunst_${cmd}"
-id=$(cat $id_file 2>/dev/null)
+id=$(eval "cat $id_file 2>/dev/null")
 message=$(eval "$1 2>/dev/null")
 if [[ ! $message ]]; then
   message=$(eval "~/.scripts/${cmd}.sh")
 fi
 
+echo $id
 if [[ $id ]]; then
   eval "dunstify -p -r $id \"$message\" > $id_file"
 else
