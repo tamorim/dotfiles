346dad542478e364a9b7be2a0180d3c83d874e2f
Thor Amorim <thoramorim@gmail.com>
2016-11-07 15:09:40 -0200

    Remove eval from ntf script

--- .scripts/ntf.sh
+++ .scripts/ntf.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-cmd=$(sed -n 's/^\(.*\)\s\?.*$/\1/p' <<< "$1")
-id_file="~/.dunst_${cmd}"
-id=$(eval "cat $id_file 2>/dev/null")
-message=$(eval "$1 2>/dev/null")
+cmd=$(sed -n 's/^\(\w*\)\s\?.*$/\1/p' <<< "$1")
+id_file=~/.dunst_$cmd
+id=$(cat $id_file 2>/dev/null)
+message=$($1 2>/dev/null)
 if [[ ! $message ]]; then
-  message=$(eval "~/.scripts/${cmd}.sh")
+  message=$(~/.scripts/$cmd.sh)
 fi
 
 if [[ $id ]]; then
-  eval "dunstify -p -r $id \"$message\" > $id_file"
+  dunstify -p -r $id "$message" > $id_file
 else
-  eval "dunstify -p \"$message\" > $id_file"
+  dunstify -p "$message" > $id_file
 fi
