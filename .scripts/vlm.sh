92f0addf7ff22094fd1f0267c4d85db825a766c5
Thor Amorim <thoramorim@gmail.com>
2016-11-07 13:12:40 -0200

    Change string parsing on vlm script

--- .scripts/vlm.sh
+++ .scripts/vlm.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-unmuted=$(amixer get Master | sed -n 'N;s/^.*\[\(\w*\)\]$/\1/p')
+unmuted=$(amixer get Master | grep '\[off\]')
 
-if [ $unmuted == "off" ]; then
+if [[ -n $unmuted ]]; then
   echo ""
 else
-  volume=$(amixer get Master | sed -n 'N;s/^.*\[\([0-9]\+%\).*$/\1/p')
+  volume=$(amixer get Master | grep -o -m 1 '\[[0-9]\+%\]' | sed -n 's/^\[\([0-9]\+%\)\]$/\1/p')
   if [[ $volume =~ ^(100|[5-9][0-9])%$ ]]; then
     echo "   ${volume}"
   elif [[ $volume =~ ^([1-4][0-9]|[1-9])%$ ]]; then
