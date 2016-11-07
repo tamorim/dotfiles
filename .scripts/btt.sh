7ef0019aee2b40e3aadd6828ae2125726665b49c
Thor Amorim <thoramorim@gmail.com>
2016-11-07 16:02:12 -0200

    Fix btt script comparison on 100

--- .scripts/btt.sh
+++ .scripts/btt.sh
@@ -2,10 +2,10 @@
 BATC=$(cat /sys/class/power_supply/BAT0/capacity)
 BATS=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ $BATS = "Charging" ]; then
+if [[ $BATS = "Charging" ]]; then
   echo "   ${BATC}"
 else
-  if [ $BATC = "100%" ]; then
+  if [[ $BATC = "100" ]]; then
     echo "   ${BATC}"
   elif [[ $BATC =~ ^([8-9][0-9]|7[5-9])$ ]]; then
     echo "   ${BATC}"
