0ba1988f82f82b9b1ccd644dd5d0c5b189f3a8a0
Thor Amorim <thoramorim@gmail.com>
2017-02-15 13:23:01 -0200

    Refactor btt script to accept arguments

--- .scripts/btt.sh
+++ .scripts/btt.sh
@@ -1,19 +1,46 @@
 #!/bin/sh
+set -e
+
 BATC=$(cat /sys/class/power_supply/BAT0/capacity)
 BATS=$(cat /sys/class/power_supply/BAT0/status)
 
-if [[ $BATS = "Charging" ]]; then
-  echo "   ${BATC}"
-else
-  if [[ $BATC = "100" ]]; then
-    echo "   ${BATC}"
-  elif [[ $BATC =~ ^([8-9][0-9]|7[5-9])$ ]]; then
-    echo "   ${BATC}"
-  elif [[ $BATC =~ ^(7[0-4]|[5-6][0-9])$ ]]; then
-    echo "   ${BATC}"
-  elif [[ $BATC =~ ^([2-4][0-9]|1[1-9])$ ]]; then
-    echo "   ${BATC}"
+function has_battery () {
+  if [ -n "$(ls /sys/class/power_supply)" ]; then
+    return 0
   else
-    echo "   ${BATC}"
+    return 1
   fi
-fi
+}
+
+function capacity () {
+  echo "$BATC"
+}
+
+function status () {
+  echo "$BATS"
+}
+
+function format () {
+  if [ has_battery ]; then
+    if [ "$BATS" = "Charging" ]; then
+      echo "   ${BATC}"
+    else
+      if [ "$BATC" = "100" ] || (($BATC > 89)); then
+        echo "   ${BATC}"
+      elif (($BATC < 90)) && (($BATC > 69)); then
+        echo "   ${BATC}"
+      elif (($BATC < 70)) && (($BATC > 49)); then
+        echo "   ${BATC}"
+      elif (($BATC < 50)) && (($BATC > 9)); then
+        echo "   ${BATC}"
+      else
+        echo "   ${BATC}"
+      fi
+    fi
+  else
+    echo "No battery info to show"
+  fi
+}
+
+cmd=$1
+$cmd
