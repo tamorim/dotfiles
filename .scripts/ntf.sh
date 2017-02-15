362cc837a803ea963974db23f32298ff22dd5451
Thor Amorim <thoramorim@gmail.com>
2017-02-15 13:25:24 -0200

    Add args parsing capability on ntf script

--- .scripts/ntf.sh
+++ .scripts/ntf.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
-if [[ $1 != $(tr -d " " <<< "$1") ]]; then
-  cmd=$(sed -n 's/^\(.*\)\s.*$/\1/p' <<< "$1")
+if [ "$1" != "$(echo "$1" | tr -d " ")" ]; then
+  cmd=$(echo "$1" | sed -n 's/^\(.*\)\s.*$/\1/p')
+  args=$(echo "$1" | sed -n 's/^.*\s\(.*\)$/\1/p')
 else
   cmd=$1
+  args=""
 fi
 id_file=~/.dunst_$cmd
 id=$(cat $id_file 2>/dev/null)
 message=$($1 2>/dev/null)
-if [[ ! $message ]]; then
-  message=$(~/.scripts/$cmd.sh)
+if [ ! "$message" ]; then
+  message=$(~/.scripts/$cmd.sh $args)
 fi
 
-if [[ $id ]]; then
+if [ "$id" ]; then
   dunstify -p -r $id "$message" > $id_file
 else
   dunstify -p "$message" > $id_file
