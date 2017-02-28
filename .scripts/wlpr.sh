a4cecce3dc07b31ce528f00f64ce5f9ebff046f1
Thor Amorim <thoramorim@gmail.com>
2017-02-28 00:59:00 -0300

    Improve wlpr script

--- .scripts/wlpr.sh
+++ .scripts/wlpr.sh
@@ -3,10 +3,10 @@
 base_dir=~/Pictures
 
 function main () {
-  file="$(echo $base_dir/*-wlpr.*)"
-  if [ -e "$file" ]; then
-    render_mode=$(basename "$file" | sed -n 's/^\(.*\)-.*$/\1/p')
-    feh --bg-$render_mode $file
+  file="$(ls $base_dir | grep -e '^\(fill\|tile\|center\|max\|scale\)-wlpr.\(jpe\?g\|png\|gif\)$')"
+  if [ -n "$file" ]; then
+    render_mode="$(echo $file | sed -n 's/^\(.*\)-.*$/\1/p')"
+    feh --bg-$render_mode "$base_dir/$file"
   fi
 }
 
